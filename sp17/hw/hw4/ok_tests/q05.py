b7e0f513b1fccbffdaf78442ce7d1cb26ec547c4
Joseph Simonian <j.simonian10@gmail.com>
2017-03-13 12:46:52 -0700

    Hotfix q5 test

--- sp17/hw/hw4/ok_tests/q05.py
+++ sp17/hw/hw4/ok_tests/q05.py
@@ -6,8 +6,11 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> connection.execute(query_q5).fetchall() == [('BUSH, JEB', 'RUBIO, MARCO'), ('RUBIO, MARCO', 'BUSH, JEB')]
-          True                                                                       
+          >>> student_sol = connection.execute(query_q5).fetchall()
+          >>> oldsol = (student_sol == [('BUSH, JEB', 'RUBIO, MARCO'), ('RUBIO, MARCO', 'BUSH, JEB')])
+          >>> newsol = (len(student_sol) == 8) # correct usage of state field in comm / inter_comm
+          >>> oldsol or newsol
+          True
                       """,
           'hidden': False,
           'locked': False
