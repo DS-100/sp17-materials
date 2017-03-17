fc9e388c56317791ca881880127165023af1b9c8
hmilner <hmilner@conviva.com>
2017-03-16 23:48:04 -0700

    sp17/labs/lab09.push

---
+++ sp17/labs/lab09/ok_tests/q3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {  
+          'code': r"""
+          >>> .74 < average_scandals_per_month < .76
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
