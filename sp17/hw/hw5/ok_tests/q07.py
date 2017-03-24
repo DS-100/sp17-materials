991c8071d441952bfb9d8acfc2811d4b750475bb
Henry Milner <henry@conviva.com>
2017-03-24 12:47:19 -0700

    sp17/hw/hw5.push

--- sp17/hw/hw5/ok_tests/q07.py
+++ sp17/hw/hw5/ok_tests/q07.py
@@ -18,7 +18,7 @@ test = {
           >>> 190 < int(np.std(obs_q)) < 220
           True
           >>> # Test their interaction
-          >>> -30 < int(np.mean(pdf_q - obs_q)) < 0
+          >>> 0 < int(np.mean(pdf_q - obs_q)) < 30
           True
           >>> 20 < int(np.std(pdf_q - obs_q)) < 40
           True    
