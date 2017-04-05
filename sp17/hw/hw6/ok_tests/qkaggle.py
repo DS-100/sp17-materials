946019d34599b34b705de7e3e63a1240ab788f52
Do <do@berkeley.edu>
2017-04-05 05:39:46 -0700

    sp17/hw/hw6.push

---
+++ sp17/hw/hw6/ok_tests/qkaggle.py
@@ -0,0 +1,21 @@
+test = {
+  'name': 'Question 1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {  
+          'code': r"""
+          >>> my_test_RMSE <= 40000
+          True""",
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
